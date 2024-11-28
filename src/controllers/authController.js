e3c77ecd59dc0191bc782c2f53e953d13d8c75da
CLINTPS <clintps1010@gmail.com>
2024-11-28 19:08:16 +0530

    Complited

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -31,7 +31,7 @@ const login = async (req,res)=> {
         const token= jwt.sign({ userId: user._id, role: user.role },config.JWT_SECRET, {
             expiresIn:config.JWT_EXPIRES_IN,
         })
-        res.status(200).json({token});
+        res.status(200).json({message:"Logined",token});
     } catch (err) { 
         res.status(500).json({ message: 'Error logging in', error: err.message });
     }
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 const config = require('../config/config')
 
 const verifyToken = (req,res,next) => {
+    console.log("Reached verify token",req.headers.authorization);
+    
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) return res.status(401).json({ message: 'Unauthorized' });
     jwt.verify(token,config.JWT_SECRET,(err,decoded)=>{
